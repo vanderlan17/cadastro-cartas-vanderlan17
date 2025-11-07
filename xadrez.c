3820928177cb9a422a95ded1b917f4a72031584a
vanderlan17 <cauasobral.2013@gmail.com>
2025-11-07 23:51:36 +0000

    criei o printf

--- xadrez.c
+++ xadrez.c
@@ -4,18 +4,93 @@
 
        printf("Desafio xadrez! \n");
        printf("Novo commit \n");
-       
-       
-       
+     
+    char estado[1];
+    char codigo_da_carta[10];
+    char nome_da_cidade[20];
+    int populacao[20];
+    float area[20];
+    float pib[20];
+    int pontos_turisticos[10];
 
-      
-      
+    printf("Estado: ");
+    scanf("%s", estado);
 
+    printf("Código da carta: ");
+    scanf("%s", codigo_da_carta);
 
+    printf("Nome da cidade: ");
+    scanf("%s", nome_da_cidade);
 
+    printf("População: ");
+    scanf("%d", &populacao);
 
+    printf("Área (em km²): ");
+    scanf("%f", &area);
 
+    printf("PIB: ");
+    scanf("%f", &pib);
 
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
 
+    printf("\n--- Dados da cidade ---\n");
+    printf("Estado: %s\n", estado);
+    printf("Código da carta: %s\n", codigo_da_carta);
+    printf("Nome da cidade: %s\n", nome_da_cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos turísticos: %d\n", pontos_turisticos);
 
+    
+     
+     char estado[1];
+    char codigo_da_carta[10];
+    char nome_da_cidade[20];
+    int populacao[20];
+    float area[20];
+    float pib[20];
+    int pontos_turisticos[10];
+
+    printf("Estado: ");
+    scanf("%s", estado);
+
+    printf("Código da carta: ");
+    scanf("%s", codigo_da_carta);
+
+    printf("Nome da cidade: ");
+    scanf("%s", nome_da_cidade);
+
+    printf("População: ");
+    scanf("%d", &populacao);
+
+    printf("Área (em km²): ");
+    scanf("%f", &area);
+
+    printf("PIB: ");
+    scanf("%f", &pib);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos);
+
+    printf("\n--- Dados da cidade ---\n");
+    printf("Estado: %s\n", estado);
+    printf("Código da carta: %s\n", codigo_da_carta);
+    printf("Nome da cidade: %s\n", nome_da_cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos turísticos: %d\n", pontos_turisticos);
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
